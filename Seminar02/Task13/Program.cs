b1f8c9a9813b275b651f6cef1ca917470ca93688
SergeyXP <s.pusthotin@gmail.com>
2022-08-22 23:42:22 +0400

    Seminar02/Task13

--- Seminar02/Task13/Program.cs
+++ Seminar02/Task13/Program.cs
@@ -3,3 +3,16 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
+Console.WriteLine("Task 13");
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number < 0)
+    Console.WriteLine("Ошибка! Необходимо ввести положительное число");
+else
+{
+    if (number < 100)
+        Console.WriteLine("третьей цифры нет");
+    else
+        Console.WriteLine(number / 100 % 10);
+}
