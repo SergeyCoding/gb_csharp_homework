3f7b29dfbdbdfbbbfdea4a3f2c72b698d8c3bddf
SergeyXP <s.pusthotin@gmail.com>
2022-08-25 23:57:43 +0400

    Task19

--- Seminar03/Task19/Program.cs
+++ Seminar03/Task19/Program.cs
@@ -9,25 +9,12 @@ Console.WriteLine("Task 19");
 Console.Write("Введите пятизначное число: ");
 int number = int.Parse(Console.ReadLine());
 
-if (number == 0)
-{
-    number = new Random().Next((int)Math.Pow(10, 4), (int)Math.Pow(10, 5) - 1);
-    System.Console.WriteLine((int)Math.Pow(10, 6) - 1);
-}
-
-System.Console.WriteLine(number);
-
 if (number < 10000 || number > 99999)
     Console.WriteLine("Ошибка! Необходимо ввести пятизначное число");
 else
 {
-    System.Console.WriteLine(number);
-
-    int c1 = number / 10_000;
-    int c2 = number / 1_000 % 10;
-
-    System.Console.WriteLine(c1);
-    System.Console.WriteLine(c2);
+    int c1 = number / 10000;
+    int c2 = number / 1000 % 10;
 
     if (number % 100 == c2 * 10 + c1)
         Console.WriteLine("да");
