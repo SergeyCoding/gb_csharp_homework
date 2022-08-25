3afe9d52be0df509751697bf49b54bb29d2ed09b
SergeyXP <s.pusthotin@gmail.com>
2022-08-26 00:09:09 +0400

    Task21

--- Seminar03/Task21/Program.cs
+++ Seminar03/Task21/Program.cs
@@ -3,19 +3,25 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
+#pragma warning disable CS8604 
 Console.Write("Введите X1: ");
 int x1 = int.Parse(Console.ReadLine());
 Console.Write("Введите Y1: ");
 int y1 = int.Parse(Console.ReadLine());
+Console.Write("Введите Z1: ");
+int z1 = int.Parse(Console.ReadLine());
 
 Console.Write("Введите X2: ");
 int x2 = int.Parse(Console.ReadLine());
 Console.Write("Введите Y2: ");
 int y2 = int.Parse(Console.ReadLine());
+Console.Write("Введите Z2: ");
+int z2 = int.Parse(Console.ReadLine());
 
-double dist = Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+int dx = x1 - x2;
+int dy = y1 - y2;
+int dz = z1 - z2;
 
-// округление
-//dist = Math.Floor(dist * 100) / 100;
+double dist = Math.Sqrt(dx * dx + dy * dy + dz * dz);
 
 Console.WriteLine($"Расстояние: {dist:f2}");
