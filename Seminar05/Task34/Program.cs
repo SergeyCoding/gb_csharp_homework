02187d0bfc4a76bce7d82ab865469ea5986e1f98
SergeyXP <s.pusthotin@gmail.com>
2022-09-01 23:53:17 +0400

    Task34

--- Seminar05/Task34/Program.cs
+++ Seminar05/Task34/Program.cs
@@ -13,41 +13,25 @@ int[] GetArray(int size, int minValue, int maxValue)
     return res;
 }
 
-void Proizv(int[] arr)
+int EvenCount(int[] array)
 {
-    for (int i = 0; i < arr.Length / 2 + arr.Length % 2; i++)
+    int counter = 0;
+    foreach (var item in array)
     {
-        int k = arr.Length - 1 - i;
-        if (arr.Length % 2 == 1 && i == k)
-            Console.Write($"{arr[i]} ");
-        else
-            Console.Write($"{arr[i] * arr[k]} ");
+        if (item % 2 == 0)
+            counter++;
     }
+
+    return counter;
 }
-
-int[] ProductionArray(int[] array)
-{
-    int size = array.Length / 2;
-
-    if (array.Length % 2 != 0) size++;
-    int[] result = new int[size];
-    for (int i = 0; i < size; i++)
-    {
-        result[i] = array[i] * array[array.Length - 1 - i];
-    }
-    if (array.Length % 2 != 0) result[size - 1] = array[array.Length / 2];
-    return result;
-
-}
-
-// если поставить диапазон от 10 до 99, тогда все попадут
-int[] myArray = GetArray(10, -10, 100);
-// myArray = new int[] { 1, 2 };
-Console.WriteLine(String.Join(" ", myArray));
-
-Proizv(myArray);
-System.Console.WriteLine();
-Console.WriteLine(String.Join(" ", ProductionArray(myArray)));
 
 #pragma warning disable CS8604 
-Console.WriteLine("Task 25");
+Console.WriteLine("Task 34");
+
+Console.Write("Введите размер массива: ");
+int numCount = int.Parse(Console.ReadLine());
+
+int[] arr = GetArray(numCount, 10, 99);
+Console.WriteLine(String.Join(", ", arr));
+
+Console.WriteLine($"Четных: {EvenCount(arr)}");
