b8f6dc4e683db06b79b6b710acf4005e2053d78e
SergeyXP <s.pusthotin@gmail.com>
2022-09-02 00:01:47 +0400

    Task36

--- Seminar05/Task36/Program.cs
+++ Seminar05/Task36/Program.cs
@@ -2,3 +2,36 @@
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+int OddPosSum(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 == 1)
+            sum = sum + array[i];
+    }
+
+    return sum;
+}
+
+#pragma warning disable CS8604 
+Console.WriteLine("Task 36");
+
+Console.Write("Введите размер массива: ");
+int numCount = int.Parse(Console.ReadLine());
+
+int[] arr = GetArray(numCount, -100, 100);
+Console.WriteLine(String.Join(", ", arr));
+
+Console.WriteLine($"Сумма на нечетных позициях: {OddPosSum(arr)}");
