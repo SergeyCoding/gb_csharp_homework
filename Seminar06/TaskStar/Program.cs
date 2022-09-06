f84144dc31e944a9f66801b94f206ebc13171a17
SergeyXP <s.pusthotin@gmail.com>
2022-09-06 12:37:33 +0400

    Task Star

--- Seminar06/TaskStar/Program.cs
+++ Seminar06/TaskStar/Program.cs
@@ -14,6 +14,7 @@ int[,] GetArray(int rows, int cols, int minValue, int maxValue)
     }
     return res;
 }
+
 void PrintArray(int[,] array)
 {
     for (int row = 0; row < array.GetLength(0); row++)
@@ -32,9 +33,9 @@ int[] GetLineArray(int[,] arr, int row, int col, int drow, int dcol, int count)
     int[] result = new int[count];
     for (int i = 0; i < count; i++)
     {
-        result[i] = arr[row, col];
         row = row + drow;
         col = col + dcol;
+        result[i] = arr[row, col];
     }
     return result;
 }
@@ -44,35 +45,27 @@ int[] FlatArray(int[,] arr)
     int[] result = new int[arr.Length];
 
     int row = arr.GetLength(0) - 1;
-    int col = 0;
+    int col = -1;
     var rowCount = arr.GetLength(0) - 1;
     var colCount = arr.GetLength(1);
+    var drow = -1;
+    var dcol = 1;
 
-    while (rowCount > 0 && colCount > 0)
+    while (rowCount > 0 || colCount > 0)
     {
-        result = GetLineArray(arr, row, col, 0, 1, colCount);
+        result = GetLineArray(arr, row, col, 0, dcol, colCount);
+        Console.WriteLine(string.Join(", ", result));
+        col = col + dcol * colCount;
+
+        result = GetLineArray(arr, row, col, drow, 0, rowCount);
+        Console.WriteLine(string.Join(", ", result));
+        row = row + drow * rowCount;
+
         colCount = colCount - 1;
-        row = row - 1;
-        col = col + colCount;
-        Console.WriteLine(string.Join(", ", result));
-
-        result = GetLineArray(arr, row, col, -1, 0, rowCount);
         rowCount = rowCount - 1;
-        row = row - rowCount;
-        col = col - 1;
-        Console.WriteLine(string.Join(", ", result));
 
-        result = GetLineArray(arr, row, col, 0, -1, colCount);
-        colCount = colCount - 1;
-        row = row + 1;
-        col = col - colCount;
-        Console.WriteLine(string.Join(", ", result));
-
-        result = GetLineArray(arr, row, col, 1, 0, rowCount);
-        rowCount = rowCount - 1;
-        row = row + rowCount;
-        col = col + 1;
-        Console.WriteLine(string.Join(", ", result));
+        drow = -drow;
+        dcol = -dcol;
     }
 
     return result;
@@ -83,9 +76,9 @@ int[] FlatArray(int[,] arr)
 Console.WriteLine("Task 6-Star");
 
 Console.Write("Введите rows: ");
-var rows = 4;// int.Parse(Console.ReadLine());
+var rows = 3;// int.Parse(Console.ReadLine());
 Console.Write("Введите cols: ");
-var cols = 5;//int.Parse(Console.ReadLine());
+var cols = 4;//int.Parse(Console.ReadLine());
 
 Console.WriteLine();
 
