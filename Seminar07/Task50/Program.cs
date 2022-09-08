137b90aef44d459906d902bbf75312d99651f6ce
SergeyXP <s.pusthotin@gmail.com>
2022-09-08 23:38:27 +0400

    Task50

--- Seminar07/Task50/Program.cs
+++ Seminar07/Task50/Program.cs
@@ -1,6 +1,78 @@
-﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает 
+// значение этого элемента или же указание, что такого элемента нет.
 // Например, задан массив:
 // 1 4 7 2
 // 5 9 2 3
 // 8 4 2 4
 // 17 -> такого числа в массиве нет
+
+int[,] GetArray(int rows, int cols, int minValue, int maxValue)
+{
+    int[,] res = new int[rows, cols];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            res[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return res;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int row = 0; row < array.GetLength(0); row++)
+    {
+        for (int col = 0; col < array.GetLength(1); col++)
+        {
+            Console.Write($"{array[row, col],5}");
+        }
+
+        Console.WriteLine();
+    }
+}
+
+bool FindNumInArray(int[,] arr, int num, int[] coord)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (arr[i, j] == num)
+            {
+                coord[0] = i;
+                coord[1] = j;
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+#pragma warning disable CS8604 
+Console.WriteLine("Task 50");
+
+Console.Write("Введите rows: ");
+var rows = int.Parse(Console.ReadLine());
+Console.Write("Введите cols: ");
+var cols = int.Parse(Console.ReadLine());
+Console.Write("Введите число: ");
+var number = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+if (rows < 1 || cols < 1)
+{
+    Console.WriteLine("Ошибка! Введите корректные данные");
+    return;
+}
+
+var arr = GetArray(rows, cols, 1, 10);
+var coord = new int[2];
+PrintArray(arr);
+Console.WriteLine();
+
+if (FindNumInArray(arr, number, coord))
+    Console.WriteLine($"({coord[0]}; {coord[1]})");
+else
+    Console.WriteLine("такого числа в массиве нет");
