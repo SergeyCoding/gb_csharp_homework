014ca8eb2dbe86874c0566eca6cf6368c5a9f810
SergeyXP <s.pusthotin@gmail.com>
2022-09-16 01:08:33 +0400

    Seminar09

--- Seminar09/TaskStar/Program.cs
+++ Seminar09/TaskStar/Program.cs
@@ -3,3 +3,52 @@
 // Пример правильной скобочной последовательности: (), ()[]{}
 // Пример неправильной: (], ({)}
 // Написать программу, которая определяет правильная ли скобочная последовательность была введена
+
+var bracketOpen = new[] { '{', '[', '(' };
+var bracketClose = new[] { '}', ']', ')' };
+
+bool Brackets(string s)
+{
+    var stack = new Stack<char>();
+
+    foreach (var c in s)
+    {
+        for (int i = 0; i < bracketOpen.Length; i++)
+        {
+            if (bracketOpen[i] == c)
+                stack.Push(c);
+        }
+
+        for (int i = 0; i < bracketOpen.Length; i++)
+        {
+            if (bracketClose[i] == c)
+            {
+                if (stack.Count == 0 || stack.Pop() != bracketOpen[i])
+                    return false;
+            }
+        }
+    }
+
+    return stack.Count == 0;
+}
+
+#pragma warning disable CS8604 
+Console.WriteLine("Task 9-Star");
+
+Console.Write("Выражение: ");
+var str = Console.ReadLine();
+PrintResult(str);
+
+
+//PrintResult("())");
+//PrintResult("()()");
+//PrintResult("()[{]");
+//PrintResult("{()[]}");
+//PrintResult(")())");
+//PrintResult("())");
+//PrintResult("[()({{{}}})]");
+
+void PrintResult(string str)
+{
+    Console.WriteLine($"{str} - {Brackets(str)}");
+}
