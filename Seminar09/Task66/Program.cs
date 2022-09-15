0c1ee283583557747f2eae12bad15b12ee7003d0
SergeyXP <s.pusthotin@gmail.com>
2022-09-15 23:19:50 +0400

    Task66

--- Seminar09/Task66/Program.cs
+++ Seminar09/Task66/Program.cs
@@ -2,3 +2,22 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
+int Sum(int m, int n)
+{
+    if (m == n)
+        return m;
+
+    return m + Sum(m + 1, n);
+}
+
+#pragma warning disable CS8604 
+Console.WriteLine("Task 66");
+
+Console.Write("Введите M: ");
+var m = int.Parse(Console.ReadLine());
+Console.Write("Введите N: ");
+var n = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+System.Console.WriteLine(Sum(m, n));
+
